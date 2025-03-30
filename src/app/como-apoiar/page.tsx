d542dcd7f06f0068057bf9fa704526c7dae5bd12
Rafael Oliveira Lopes <rafaellopes.dev@gmail.com>
2025-03-30 20:36:52 -0300

    chore(pages): add new pages folders for Como Apoiar, Fale Conosco, Galeria de Imagens, Nossa Equipe, Nossa História, Nosso Trabalho, and Vídeos

---
+++ src/app/como-apoiar/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="bg-light-chocolate content-center justify-center p-7 align-middle">
+      <h1 className={'text-3xl'}>Como Apoiar</h1>
+    </main>
+  );
+}
