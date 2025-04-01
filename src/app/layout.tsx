97b5e0793dd3c908aeaa8fe4bd9a2cd1b1921b9d
Rafael Oliveira Lopes <rafaellopes.dev@gmail.com>
2025-04-01 09:26:56 -0300

    feat(footer): add footer component and update icon paths
    Included FontAwesome dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Navbar from './components/Navbar';
+import Footer from './components/footer';
 
 export const metadata: Metadata = {
   title: 'Associação da Comunidade Quilombola Urbana João Borges Vieira',
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className={`bg-snow antialiased`}>
         <Navbar />
         <main className="mx-auto w-full flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
