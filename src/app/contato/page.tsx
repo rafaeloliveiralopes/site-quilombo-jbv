b4e9111322f802b0dc3a35a1862cc45b2cfbfeb4
Rafael Oliveira Lopes <rafaellopes.dev@gmail.com>
2025-04-10 16:16:01 -0300

    feat(form): create form component for the contact page

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,7 +1,12 @@
+import Form from '../components/Form';
+
 export default function Page() {
   return (
-    <main className="bg-light-chocolate content-center justify-center p-7 align-middle">
-      <h1 className={'text-3xl'}>Fale Conosco</h1>
-    </main>
+    <>
+      <div className="bg-light-chocolate w-full px-6 py-6 text-2xl tracking-tight md:px-8 md:text-4xl">
+        <h1>Fale Conosco</h1>
+      </div>
+      <Form />
+    </>
   );
 }
